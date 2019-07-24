08f98912ed9f5ebd6c56cc9868d9039e59db0cc8
Olivia D'Angelo <olivia.dangelo@gmail.com>
2019-07-23 22:10:58 -0400

    added test models

--- fouryearsago/fouryearsago/settings.py
+++ fouryearsago/fouryearsago/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'fya.apps.FyaConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
